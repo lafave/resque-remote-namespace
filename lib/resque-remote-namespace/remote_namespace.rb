c07f2d7e5411a30eba6c08a08942823ed349543c
Alex Sharp <ajsharp@gmail.com>
2013-01-19 11:17:04 -0800

    DRY up en/de-queuing with a block helper

--- lib/resque-remote-namespace/remote_namespace.rb
+++ lib/resque-remote-namespace/remote_namespace.rb
@@ -10,12 +10,9 @@ module Resque
       # @param [ String ] klass The class name of the job to enqueue.
       # @param [ Array ] args The arguments to the job being remotely enqueued.
       def remote_enqueue_to(queue, namespace, klass, *args)
-        original_namespace      = Resque.redis.namespace
-        Resque.redis.namespace = namespace
-
-        Resque::Job.create(queue.to_sym, klass, *args)
-      ensure
-        Resque.redis.namespace = original_namespace
+        temp_namespace(namespace) do
+          Resque::Job.create(queue.to_sym, klass, *args)
+        end
       end
 
       # Remote dequeue of a job by temporarily changin gthe Resque redis
@@ -26,10 +23,25 @@ module Resque
       # @param [ String ] klass The class name of the job to enqueue.
       # @param [ Array ] args The arguments to the job being remotely dequeued.
       def remote_dequeue_from(queue, namespace, klass, *args)
-        original_namespace      = Resque.redis.namespace
-        Resque.redis.namespace = namespace
+        temp_namespace(namespace) do
+          Resque::Job.destroy(queue.to_sym, klass, *args)
+        end
+      end
 
-        Resque::Job.destroy(queue.to_sym, klass, *args)
+      private
+
+      # Temporarily change the Resque namespace
+      #
+      # @example
+      #   temp_namespace('my-temp-namespace') do
+      #     Resque.redis.destroy_all_the_things
+      #   end
+      #
+      # @param [String] namespace the temporary namespace
+      def temp_namespace(namespace)
+        original_namespace = Resque.redis.namespace
+        Resque.redis.namespace = namespace
+        yield
       ensure
         Resque.redis.namespace = original_namespace
       end
