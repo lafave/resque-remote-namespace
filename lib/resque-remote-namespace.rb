0289ee852b6387a42b4baae5abd7bfaf21576502
Zachary Kim <zachary.kim@gmail.com>
2014-06-12 22:56:26 -0700

    Update to use resque-scheduler 3.0
    * `resque_scheduler` -> `resque-scheduler`
    * Bump deps

--- lib/resque-remote-namespace.rb
+++ lib/resque-remote-namespace.rb
@@ -1,4 +1,4 @@
 require 'resque'
-require 'resque_scheduler'
+require 'resque-scheduler'
 require 'resque-remote-namespace/remote_namespace'
 require 'resque-remote-namespace/support/shared_helpers'
